c1aa0d651eac4b6e77f9773a3ae912b9b0420c6d
Maciej Chudak <macioosch@gmail.com>
2013-08-15 17:50:05 +0200

    WIP: graphing pressure while the simulations run, but for that the logging mechanism needs a little rehaul

---
+++ graph_pressure.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""
+    For this to work, logging must be config-wise, not pid-wise!
+"""
+from glob import glob
+from pprint import pprint
+import numpy as np
+import re
+
+# local imports
+from my_pressure import pressure
+
+status_regexp_events = "Events ([0-9]+)k, t ([0-9\.]+), "
+
+partial_results = []
+n_atoms = None
+
+for file_name in glob("stdouterr/std.out.*"):
+    with open(file_name, "r") as input_f:
+        # add a new array for results from another process
+        partial_results.append([])
+        collision_offset = 0
+        time_offset = 0.0
+
+        for line in input_f:
+            if n_atoms is None:
+                n_atoms = re.search("Particle Count ([0-9]+)$", line)
+                if n_atoms:
+                    n_atoms = int(n_atoms.group(1))
+                    break
+
+            line_results = re.search(status_regexp_events, line)
+
+            if line_results:
+                events, sim_time = line_results.groups()
+
+                events = 1000 * int(events) + collision_offset
+                sim_time = float(sim_time) + time_offset
+
+                if len(partial_results[-1]) >= 1 \
+                        and events < partial_results[-1][-1][1]:
+                    collision_offset = partial_results[-1][-1][1]
+                    time_offset = partial_results[-1][-1][2]
+
+                partial_results[-1].append([events, sim_time,
+                    pressure(n_atoms, events, sim_time)])
+
+pprint(partial_results)
