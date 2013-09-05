59d57cc28c99640e0453a2445dd329840ff84743
Maciej Chudak <macioosch@gmail.com>
2013-09-05 17:44:37 +0200

    more helper functions

--- pressure-plot.py
+++ pressure-plot.py
@@ -2,7 +2,6 @@
 # encoding=utf-8
 from __future__ import division, print_function, unicode_literals
 
-from scipy.sparse import csr_matrix
 from uncertainties import unumpy as unp, ufloat
 from xml.dom import minidom
 import bz2
@@ -11,39 +10,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # local imports
-from my_helper_functions import my_pressure
-
-def nufd(x):
-    # D f = f'
-    n = len(x)
-    h = x[1:]-x[:n-1]
-    a0 = -(2*h[0]+h[1])/(h[0]*(h[0]+h[1]))
-    ak = -h[1:]/(h[:n-2]*(h[:n-2]+h[1:]))
-    an = h[-1]/(h[-2]*(h[-1]+h[-2]))
-    b0 = (h[0]+h[1])/(h[0]*h[1]) 
-    bk = (h[1:] - h[:n-2])/(h[:n-2]*h[1:])
-    bn = -(h[-1]+h[-2])/(h[-1]*h[-2])
-    c0 = -h[0]/(h[1]*(h[0]+h[1]))
-    ck = h[:n-2]/(h[1:]*(h[:n-2]+h[1:]))
-    cn = (2*h[-1]+h[-2])/(h[-1]*(h[-2]+h[-1]))
-    val = np.hstack((a0,ak,an,b0,bk,bn,c0,ck,cn))
-    row = np.tile(np.arange(n),3)
-    dex = np.hstack((0,np.arange(n-2),n-3))
-    col = np.hstack((dex,dex+1,dex+2))
-    D = csr_matrix((val,(row,col)),shape=(n,n))
-    return D
-
-def uderivative(x0, y0):
-    x1 = 0.5 * (x0[:-1] + x0[1:])
-    y1 = np.diff(y0) / np.diff(x0)
-    return x1, y1
-
-def uplot(x, y, derivative=0, fmt='.-'):
-    for i in range(derivative):
-        x, y = uderivative(x, y)
-    yn = [ i.n for i in y ]
-    ys = [ i.s for i in y ]
-    plt.errorbar(x, yn, fmt=fmt, yerr=ys)
+from my_helper_functions import *
 
 varying_parameters = ["pressures_virial", "pressures_collision", "msds_val",
         "msds_diffusion", "times"]
