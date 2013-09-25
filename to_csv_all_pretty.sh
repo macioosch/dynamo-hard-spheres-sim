4d18c69b9990279e3a56bd6ee85a6a3e15b0e9e8
Maciej Chudak <macioosch@gmail.com>
2013-09-25 15:24:58 +0200

    csv pretty update

--- to_csv_all_pretty.sh
+++ to_csv_all_pretty.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 FILES='results/*_219700000_1098500000.xml.bz2 results/*_2097152000_6291456000.xml.bz2'
-OUTPUT='csv/D-vs-N-new.csv'
+OUTPUT='csv/D-vs-N-new-pretty.csv'
 #FILES='results/1098500_*_219700000_1098500000.xml.bz2'
-#OUTPUT='csv/1098500_219700000_1098500000.csv'
+#OUTPUT='csv/1098500_219700000_1098500000-pretty.csv'
 
 if [ -d ~/Dropbox ]; then
     #PREFIX='~/Dropbox/staż 2013/02-hard-spheres/'
