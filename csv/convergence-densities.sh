772b40a14fa898a9de998170ad0ce2332da31b2e
Maciej Chudak <macioosch@gmail.com>
2013-10-10 19:45:07 +0200

    more csv data

--- csv/convergence-densities.sh
+++ csv/convergence-densities.sh
@@ -5,5 +5,6 @@ for n in 256000 500000 1000188; do
         grep -F ${d}00000000 convergence-${n}.csv >> convergence-${n}-${d}.csv
     done
 done
-for f in *; do
+for f in convergence-500000*.csv; do
+    grep -v 1083333333 $f | grep -v 1025000000 | sponge $f
 done
