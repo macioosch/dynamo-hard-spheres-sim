589748da779f383b19e3b0c8a5eb6c6dfe336302
Maciej Chudak <macioosch@gmail.com>
2013-10-17 19:04:44 +0200

    to_csv_all_pretty.sh fix

--- to_csv_all_pretty.sh
+++ to_csv_all_pretty.sh
@@ -6,15 +6,16 @@ OUTPUT='csv/D-vs-N-new-pretty.csv'
 #OUTPUT='csv/1098500_219700000_1098500000-pretty.csv'
 
 if [ -d ~/Dropbox ]; then
-    #PREFIX='~/Dropbox/staż 2013/02-hard-spheres/'
+    echo "Processing results from Dropbox…"
     ./to_csv_pretty.py ~/Dropbox/staż\ 2013/02-hard-spheres/$FILES > $OUTPUT
 elif [ -d results ]; then
+    echo "Processing results from Dropbox…"
     ./to_csv_pretty.py $FILES > $OUTPUT
 else
-    print "No results found!"
+    echo "No results found!"
     exit 1
 fi
 
-#tail -n +15 $OUTPUT | head -n 25 > csv/uniform.csv
+tail -n +20 $OUTPUT | head -n 49 > csv/uniform.csv
 
 #./plot_pressure.gnuplot
