df8c5d49859833d23a5cee4dd586af190c2357c5
Maciej Chudak <macioosch@gmail.com>
2013-10-22 15:20:00 +0200

    initial new convergence results

--- convergence-csv-convert.sh
+++ convergence-csv-convert.sh
@@ -1,3 +1,5 @@
-time ./convergence-csv.py 256000 500000000 > csv/convergence-256000.csv
-time ./convergence-csv.py 500000 1000000000 > csv/convergence-500000.csv
-time ./convergence-csv.py 1000188 2000000000 > csv/convergence-1000188.csv
+time ./convergence-csv.py 256000 2560000000 > csv/convergence-256000.csv
+
+#time ./convergence-csv.py 256000 500000000 > csv/convergence-256000.csv
+#time ./convergence-csv.py 500000 1000000000 > csv/convergence-500000.csv
+#time ./convergence-csv.py 1000188 2000000000 > csv/convergence-1000188.csv
