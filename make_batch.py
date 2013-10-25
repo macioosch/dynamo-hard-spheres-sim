bafe0ac246bf5bab36ab8aeaaf4cfa6d3b4291c3
Maciej Chudak <macioosch@gmail.com>
2013-10-25 19:27:37 +0200

    make_batch.py works better

--- make_batch.py
+++ make_batch.py
@@ -4,6 +4,7 @@ from numpy import linspace, pi
 base_str = './jobber.py -q --pp "m" --p0 {0:.0f} --p1 {0:.0f} --pn 1 '\
         '--round "-p {1:.16f} -r 1 -e {2:.0f} -c {3:.0f}"'
 
-for N in (int(i) for i in 2**linspace(10,20,11)):
+for i in 2**linspace(10,20,11):
+    N = int(ceil((i/4)**(1/3.))**3*4)
     for pf in linspace(0.1,0.9,5)*pi/6:
         print(base_str.format(N, pf, N*12e3, N*6e4))
