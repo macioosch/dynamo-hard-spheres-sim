cd6227732871874e1626704eb7950c7a0743410a
Maciej Chudak <macioosch@gmail.com>
2013-10-31 16:13:59 +0100

    eta

---
+++ eta.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import datetime
+import fileinput
+import re
+
+regexp_time = ((86400," ([0-9]+)d"), (3600," ([0-9]+)hr"),
+        (60," ([0-9]+)min"), (1," ([0-9]+)s, "))
+regexp_events = "Events ([0-9]+)k,"
+
+now = datetime.datetime.now()
+
+for line in fileinput.input():
+    eta = 0
+    for seconds, pattern in regexp_time:
+        match = re.search(pattern, line)
+        if match:
+            eta += seconds*int(match.group(1))
+    collisions = 1000*int(re.search(regexp_events, line).group(1))
+    print("{:7.3f} h left, {:4.2e} collisions done, finish: {}".format(eta/3600., collisions, 
+        now + datetime.timedelta(seconds=eta)))
