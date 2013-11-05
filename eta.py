ecc6800a6999bfa611705d086518379999c05452
Maciej Chudak <macioosch@gmail.com>
2013-11-05 11:42:30 +0100

    eta.py changes

--- eta.py
+++ eta.py
@@ -1,6 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 import datetime
 import fileinput
+import pickle
 import re
 
 regexp_time = ((86400," ([0-9]+)d"), (3600," ([0-9]+)hr"),
@@ -8,6 +9,7 @@ regexp_time = ((86400," ([0-9]+)d"), (3600," ([0-9]+)hr"),
 regexp_events = "Events ([0-9]+)k,"
 
 now = datetime.datetime.now()
+etas = []
 
 for line in fileinput.input():
     eta = 0
@@ -15,6 +17,10 @@ for line in fileinput.input():
         match = re.search(pattern, line)
         if match:
             eta += seconds*int(match.group(1))
+    etas.append(eta)
     collisions = 1000*int(re.search(regexp_events, line).group(1))
-    print("{:7.3f} h left, {:4.2e} collisions done, finish: {}".format(eta/3600., collisions, 
-        now + datetime.timedelta(seconds=eta)))
+    print("{:7.3f} h left, {:4.2e} collisions done, finish: {}".format(
+            eta/3600., collisions, now + datetime.timedelta(seconds=eta)))
+
+with open("eta.pickle", "w+") as output_file:
+    pickle.dump(etas, output_file)
