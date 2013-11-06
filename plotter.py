8285f4b7aaee6ccb6eef0a05be833725d50d1809
Maciej Chudak <macioosch@gmail.com>
2013-11-06 19:06:50 +0100

    updates

--- plotter.py
+++ plotter.py
@@ -47,7 +47,13 @@ for i_index, i in enumerate(2**np.linspace(10,20,11)):
 pbar.finish()
 
 stdout_writer = csv.writer(stdout, delimiter='\t')
-stdout.write("### Data format: n_atoms\tmsds_diffusion\tstd:msds_diffusion\n")
+stdout.write(
+        "\multicolumn{1}{c}{$\\rho\\sigma^3$} \t"
+        "\multicolumn{1}{c}{$\\zeta$} \t"
+        "\multicolumn{1}{c}{$N$} \t"
+        "\multicolumn{1}{c}{$D$} \t"
+        "\multicolumn{1}{c}{$\\delta D$} \t"
+        "\multicolumn{1}{c}{runs} \n")
 
 widgets = ['Processing files: ', pb.Counter(), ' done (', pb.Percentage(),
     '), ', pb.ETA(), ' ', pb.Bar()]
@@ -129,21 +135,26 @@ for packing, subplot in izip(np.linspace(0.1, 0.9, 5) * np.pi/6,
         [321, 322, 323, 324, 325]):
     ns = [ n for n, p in izip(data["n_atoms"], data["packings"])
             if abs(p/packing - 1.0) < 1e-4 ]
-    ds = [ np.mean(d) for d, p in izip(data[graphed_parameter], data["packings"])
+    ds = [ my_mean(d) for d, p in izip(data[graphed_parameter], data["packings"])
             if abs(p/packing - 1.0) < 1e-4 ]
-    er = [ np.std(d) for d, p in izip(data[graphed_parameter], data["packings"])
+    er = [ my_means_std(d) for d, p in izip(data[graphed_parameter], data["packings"])
             if abs(p/packing - 1.0) < 1e-4 ]
+    runs = [ len(d) for d, p in izip(data[graphed_parameter], data["packings"])
+            if abs(p/packing - 1.0) < 1e-4 ]
+    zeta = [ p for p in data["packings"] if abs(p/packing - 1.0) < 1e-4 ]
+    rho = np.array(zeta) * 6./np.pi
+
     if len(ns) > 1:
         ns, ds, er = np.array(sorted(izip(ns, ds, er))).T
-
-        stdout.write("###\n### Density: {:.3f}\n###\n".format(packing*6/np.pi))
         n_ints = [ int(n) for n in ns ]
 
         # the normal csv version:
-        stdout_writer.writerows(izip(n_ints, ds, er))
-        ## the pretty version:
-        #unc_strings = [ uncertain_number_string(d, e) for d, e in izip(ds, er) ]
-        #stdout_writer.writerows(izip(n_ints, unc_strings))
+        stdout_writer.writerows(izip(rho, zeta, n_ints, ds, er, runs))
+        """
+        # the pretty version:
+        unc_strings = [ uncertain_number_string(d, e) for d, e in izip(ds, er) ]
+        stdout_writer.writerows(izip(rho, zeta, n_ints, unc_strings, runs))
+        """
 
         plt.subplot(subplot)
         plt.ylabel("$D$")
@@ -156,27 +167,35 @@ for packing, subplot in izip(np.linspace(0.1, 0.9, 5) * np.pi/6,
         # Curve fitting:
         er = [ max(1e-16*d, e) for d, e in izip(ds, er) ]
         skip = 3
-        popt, pcov = curve_fit(fit_func, 1./ns[skip:]**(1/3.), ds[skip:], [0.4, 0.1],
-                er[skip:]/np.sqrt(len(er[skip:])))
+        popt, pcov = curve_fit(fit_func, 1./ns[skip:]**(1/3.), ds[skip:],
+                [0.4, 0.1], er[skip:])
         if isinstance(pcov, collections.Iterable):
-            stderr.write("density: {}, y0 = {}, a = {}\n".format(packing*6/np.pi,
-                    uncertain_number_string(popt[0], np.sqrt(pcov[0][0])),
-                    uncertain_number_string(popt[1], np.sqrt(pcov[1][1]))))
+            #stderr.write("density: {}, y0 = {}, a = {}\n".format(packing*6/np.pi,
+            #        uncertain_number_string(popt[0], np.sqrt(pcov[0][0])),
+            #        uncertain_number_string(popt[1], np.sqrt(pcov[1][1]))))
+            stderr.write("{};{};{};{};{}\n".format(packing*6/np.pi,
+                    popt[0], np.sqrt(pcov[0][0]), popt[1], np.sqrt(pcov[1][1])))
+
+        # normal plot
         xs = np.linspace(0, 0.1, 100)
         plt.plot(xs, fit_func(xs, *popt))
-
-        plt.errorbar(1.0/ns**(1/3.), ds, fmt='.', yerr=er/np.sqrt(len(er)))
+        plt.errorbar(1.0/ns**(1/3.), ds, fmt='.', yerr=er)
 
         plt.ylim(popt[0] * np.array([0.90, 1.02]))
+        """
+        # differential plot
+        plt.errorbar(1.0/ns**(1/3.), ds - fit_func(1.0/ns**(1/3.), *popt),
+                fmt='.', yerr=er)
         
         if isinstance(pcov, collections.Iterable):
             plt.legend(["$\\rho\\sigma^3 =$ ${:.1f},$".format(packing*6/np.pi)
                     + " $D_{inf}"+"$ $=$ ${}$".format(uncertain_number_string(
                         popt[0], np.sqrt(pcov[0][0])))], loc="lower left")
+        """
 
-        #plt.plot(1.0/ns**(1/3.), er/np.sqrt(len(er)), 'o')
+        #plt.plot(1.0/ns**(1/3.), er, 'o')
 
-        #plt.errorbar(1.0/ns, ds/ds[-1], fmt='-o', yerr=er / np.sqrt(len(er)) / ds[-1])
+        #plt.errorbar(1.0/ns, ds/ds[-1], fmt='-o', yerr=er / ds[-1])
         #legend_names.append(packing)
 #plt.ylabel("Diffusion coefficient relative to the \"precise\" value: "
 #        "d(N) / d(N=1098500)")
