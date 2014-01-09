aff669437d6c73ca4e0cf2ef581a3494e167b981
Maciej Chudak <macioosch@gmail.com>
2014-01-09 15:25:14 +0100

    0.1

--- make_batch.py
+++ make_batch.py
@@ -5,11 +5,11 @@ base_str = './jobber.py -q --pp "m" --p0 {0:.0f} --p1 {0:.0f} --pn 1 '\
         '--round "-p {1:.16f} -r 1 -e {2:.0f} -c {3:.0f}"'
 
 N_min = 10
-N_sizes = 11
+N_sizes = 7
 
 for index, i in enumerate(2**linspace(N_min, N_min + N_sizes - 1, N_sizes)):
     N = int(ceil((i/4)**(1/3.))**3*4)
-    for pf in linspace(0.2, 0.8, 4)*pi/6:
+    for pf in linspace(0.1, 0.1, 1)*pi/6:
         for _ in range(2**(N_sizes - index - 1)):
             print(base_str.format(N, pf, N*12e3, N*6e4))
 
